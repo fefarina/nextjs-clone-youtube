3f027ee96ec7d900b9e600105271a27b86e5eb36
Felipe Farina <felipefarina@DESKTOP-KQRCJC2.localdomain>
2021-03-02 22:50:51 -0300

    Material-Ui Configuration

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Button } from '@material-ui/core'
 
 export default function Home() {
   return (
     <div>
       Clone Youtube com NextJS + Material UI
+      <Button variant="outlined" color="secondary">
+        Primary
+      </Button>
     </div>
   );
 }
